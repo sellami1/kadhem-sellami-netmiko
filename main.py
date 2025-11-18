6de8eeb76dd8a7f429653b2a4a7db82b9668ee58
kadhem <kadhemsellami1@gmail.com>
2025-11-18 09:45:52 +0100

    Ajout de methode acces_netmiko() et fichier .gitignore.

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("Hello, Git!")
+import getpass
+from netmiko import ConnectHandler
+
+router = {
+    "device_type": "cisco_ios",
+    "host": "sandbox-iosxr-1.cisco.com",
+    "username": "admin",
+    "password": "C1sco12345"
+}
+
+def acces_netmiko():
+    with ConnectHandler(**router) as net_connect:
+         print("Connected to remote router.")
+
+         print("Getting router's date...")
+         dte_cmd = "show clock"
+         dte_out = net_connect.send_command(dte_cmd)
+         print("Date below: ", dte_out)
+
+         print("Getting network interfaces...")
+         nic_cmd = "show ip interface brief"
+         nic_out = net_connect.send_command(nic_cmd)
+
+         with open("interface_list.txt", "w") as f:
+             f.write(nic_out)
+
+         print("Done.")
+
+print("Hello, Git! Working on it...")
+acces_netmiko()
